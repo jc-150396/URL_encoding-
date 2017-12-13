9234106adb8d3334cfc4e922d19937505b8eee01
瑞基 伊藤 <150396@jc-21.jp>
2017-12-13 14:45:18 +0900

    初回コミット

--- newApp_http/newApp_http/MainPage.xaml.cs
+++ newApp_http/newApp_http/MainPage.xaml.cs
@@ -4,14 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Net.Http;　// nugetで`System.Net.Http`か`Microsoft.AspNet.WebApi.Client`をインストすると使える。
+
+
 
 namespace newApp_http
 {
     public partial class MainPage : ContentPage
     {
+
+        public string Rakuten_URL = "https://app.rakuten.co.jp/services/api/BooksGenre/Search/20121128?format=json|&applicationId=1008026300680682252";
+
         public MainPage()
         {
             InitializeComponent();
+
+
+
+
+
+
+
         }
     }
+
+
+
+    }
+
 }
